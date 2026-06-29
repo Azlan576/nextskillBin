b275352664df25c921f8d7fcc2dfc099ae7a3e41
Azlan576 <azlannahmad34@gmail.com>
2026-06-28 22:17:10 -0700

    Add files via upload
    my

---
+++ list data structure .py
@@ -0,0 +1,33 @@
+book_list=[445,"tink and grow rich","asim",50.5]
+
+print(book_list)
+
+print(type(book_list))
+
+print(len(book_list))
+
+for x in book_list:
+    print(x)
+
+print(book_list[2])
+
+print(type(book_list[2]))
+
+print(type(book_list[3]))
+
+book_list.append("abc")
+
+print(book_list)
+
+book_list.insert(1,1997)
+
+print(book_list)
+
+book_list.remove(50.5)
+
+print(book_list)
+
+book_list.pop(2)
+
+print(book_list)
+
